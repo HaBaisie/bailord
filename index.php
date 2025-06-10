38c640f74aa586894d8307502f9737ad3ee250f5
BaisieTech <57533856+HaBaisie@users.noreply.github.com>
2025-06-11 00:42:28 +0100

    Update index.php

--- index.php
+++ index.php
@@ -917,56 +917,54 @@
                                                             </div><!-- End .row -->
                                                         </div><!-- End .intro-content -->
                                                     </div><!-- End .intro-slide -->
-                                                    </div><!-- End .intro-slider owl-carousel owl-simple -->
-                                                        <div class="intro-slide" style="background-image: url(assets/images/demos/demo-4/slider/OPPO.png);">
-                                                            <div class="container intro-content">
-                                                                <div class="row justify-content-end">
-                                                                    <div class="col-auto col-sm-7 col-md-6 col-lg-5">
-                                                                        <h3 class="intro-subtitle text-third">Deals and Promotions</h3><!-- End .h3 intro-subtitle -->
-                                                                        <h1 class="intro-title">OPPO</h1>
-                                                                        <h1 class="intro-title">A3</h1><!-- End .intro-title -->
-                                    
-                                                                        <div class="intro-price">
-                                                                            <sup class="intro-old-price">₦349,95</sup>
-                                                                            <span class="text-third">
-                                                                                ₦279<sup>.99</sup>
-                                                                            </span>
-                                                                        </div><!-- End .intro-price -->
-                                    
-                                                                        <a href="category.php?category=all" class="btn btn-primary btn-round">
-                                                                            <span>Shop More</span>
-                                                                            <i class="icon-long-arrow-right"></i>
-                                                                        </a>
-                                                                    </div><!-- End .col-lg-11 offset-lg-1 -->
-                                                                </div><!-- End .row -->
-                                                            </div><!-- End .intro-content -->
-                                                        </div><!-- End .intro-slide -->
-                                                        <div class="intro-slide" style="background-image: url(images/INIFINIX_SMART_10.png);">
-                                                            <div class="container intro-content">
-                                                                <div class="row justify-content-end">
-                                                                    <div class="col-auto col-sm-7 col-md-6 col-lg-5">
-                                                                        <h3 class="intro-subtitle text-third">Deals and Promotions</h3><!-- End .h3 intro-subtitle -->
-                                                                        <h1 class="intro-title">INIFINIX</h1>
-                                                                        <h1 class="intro-title">SMART 10</h1><!-- End .intro-title -->
-                                    
-                                                                        <div class="intro-price">
-                                                                            <sup class="intro-old-price">₦349,95</sup>
-                                                                            <span class="text-third">
-                                                                                ₦279<sup>.99</sup>
-                                                                            </span>
-                                                                        </div><!-- End .intro-price -->
-                                    
-                                                                        <a href="category.php?category=all" class="btn btn-primary btn-round">
-                                                                            <span>Shop More</span>
-                                                                            <i class="icon-long-arrow-right"></i>
-                                                                        </a>
-                                                                    </div><!-- End .col-lg-11 offset-lg-1 -->
-                                                                </div><!-- End .row -->
-                                                            </div><!-- End .intro-content -->
-                                                        </div><!-- End .intro-slide -->
-                                                </div><!-- End .intro-slider owl-carousel owl-simple -->
-                                                <span class="slider-loader"></span><!-- End .slider-loader -->
-                                        </div><!-- End .intro-slider-container -->
+                                                    <div class="intro-slide" style="background-image: url(assets/images/demos/demo-4/slider/OPPO.png);">
+                                                        <div class="container intro-content">
+                                                            <div class="row justify-content-end">
+                                                                <div class="col-auto col-sm-7 col-md-6 col-lg-5">
+                                                                    <h3 class="intro-subtitle text-third">Deals and Promotions</h3><!-- End .h3 intro-subtitle -->
+                                                                    <h1 class="intro-title">OPPO</h1>
+                                                                    <h1 class="intro-title">A3</h1><!-- End .intro-title -->
+                                                                    <div class="intro-price">
+                                                                        <sup class="intro-old-price">₦349,95</sup>
+                                                                        <span class="text-third">
+                                                                            ₦279<sup>.99</sup>
+                                                                        </span>
+                                                                    </div><!-- End .intro-price -->
+                                
+                                                                    <a href="category.php?category=all" class="btn btn-primary btn-round">
+                                                                        <span>Shop More</span>
+                                                                        <i class="icon-long-arrow-right"></i>
+                                                                    </a>
+                                                                </div><!-- End .col-lg-11 offset-lg-1 -->
+                                                            </div><!-- End .row -->
+                                                        </div><!-- End .intro-content -->
+                                                    </div><!-- End .intro-slide -->
+                                                    <div class="intro-slide" style="background-image: url(images/INIFINIX_SMART_10.png);">
+                                                        <div class="container intro-content">
+                                                            <div class="row justify-content-end">
+                                                                <div class="col-auto col-sm-7 col-md-6 col-lg-5">
+                                                                    <h3 class="intro-subtitle text-third">Deals and Promotions</h3><!-- End .h3 intro-subtitle -->
+                                                                    <h1 class="intro-title">INIFINIX</h1>
+                                                                    <h1 class="intro-title">SMART 10</h1><!-- End .intro-title -->
+                                
+                                                                    <div class="intro-price">
+                                                                        <sup class="intro-old-price">₦349,95</sup>
+                                                                        <span class="text-third">
+                                                                            ₦279<sup>.99</sup>
+                                                                        </span>
+                                                                    </div><!-- End .intro-price -->
+                                
+                                                                     <a href="category.php?category=all" class="btn btn-primary btn-round">
+                                                                        <span>Shop More</span>
+                                                                        <i class="icon-long-arrow-right"></i>
+                                                                    </a>
+                                                                </div><!-- End .col-lg-11 offset-lg-1 -->
+                                                            </div><!-- End .row -->
+                                                        </div><!-- End .intro-content -->
+                                                    </div><!-- End .intro-slide -->
+                                            </div><!-- End .intro-slider owl-carousel owl-simple -->
+                                            <span class="slider-loader"></span><!-- End .slider-loader -->
+                                    </div><!-- End .intro-slider-container -->
 
             <div class="container">
                 <h2 class="title text-center mb-4">Explore Popular Categories</h2>
