9bff02253f37aef9157f1079b4d9069e06d82e6b
BaisieTech <57533856+HaBaisie@users.noreply.github.com>
2025-06-18 15:25:33 +0100

    Update login.php

--- login.php
+++ login.php
@@ -301,11 +301,11 @@
         <div class="form sign-up">
             <?php
             if(isset($_SESSION['error'])){
-                echo "<div class='callout callout-danger text-center'><p>".$_SESSION['error']."</p></div>";
+                echo "<div class=\"callout callout-danger text-center\"><p>" . $_SESSION['error'] . "</p></div>";
                 unset($_SESSION['error']);
             }
             if(isset($_SESSION['success'])){
-                echo "<div class='callout callout-success text-center'><p>".$_SESSION['success']."</p></div>";
+                echo "<div class=\"callout callout-success text-center\"><p>" . $_SESSION['success'] . "</p></div>";
                 unset($_SESSION['success']);
             }
             ?>
@@ -347,11 +347,11 @@
         <div class="form sign-in">
             <?php
             if(isset($_SESSION['error'])){
-                echo "<div class='callout callout-danger text-center'><p>".$_SESSION['error']."</p></div>";
+                echo "<div class=\"callout callout-danger text-center\"><p>" . $_SESSION['error'] . "</p></div>";
                 unset($_SESSION['error']);
             }
             if(isset($_SESSION['success'])){
-                echo "<div class="callout callout-success text-center"><p>".$_SESSION['success']."</p></div>";
+                echo "<div class=\"callout callout-success text-center\"><p>" . $_SESSION['success'] . "</p></div>";
                 unset($_SESSION['success']);
             }
             ?>
