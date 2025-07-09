947680babc616168cbdeb618e5d48712168da1c7
BaisieTech <57533856+HaBaisie@users.noreply.github.com>
2025-07-09 17:58:04 +0100

    Update profile.php

--- profile.php
+++ profile.php
@@ -659,10 +659,10 @@
                                             <h4>Member Since:</h4>
                                         </div>
                                         <div class="col-sm-9">
-                                            <h4><?php echo htmlspecialchars($user['firstname'].' '.$user['lastname']); ?>
-					      <span class="pull-right">
-					          <a href="#editProfileModal" class="btn btn-success btn-flat btn-sm" data-toggle="modal"><i class="fa fa-edit"></i> Edit</a>
-					      </span>
+                                           <h4><?php echo htmlspecialchars($user['firstname'].' '.$user['lastname']); ?>
+					    	<span class="pull-right">
+					        	<a href="#edit" class="btn btn-success btn-flat btn-sm" data-toggle="modal" data-target="#edit"><i class="fa fa-edit"></i> Edit</a>
+					    	</span>
 					    </h4>
                                             <h4><?php echo htmlspecialchars($user['email']); ?></h4>
                                             <h4><?php echo (!empty($user['contact_info'])) ? htmlspecialchars($user['contact_info']) : 'N/a'; ?></h4>
