7cae1093c5c763a82c7676fc35241fdfbacabdaa
BaisieTech <57533856+HaBaisie@users.noreply.github.com>
2025-07-09 20:26:45 +0100

    Update category_modal.php

--- admin/includes/category_modal.php
+++ admin/includes/category_modal.php
@@ -3,24 +3,29 @@
     <div class="modal-dialog">
         <div class="modal-content">
             <div class="modal-header">
-              <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                  <span aria-hidden="true">&times;</span></button>
-              <h4 class="modal-title"><b>Add New Category</b></h4>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">×</span></button>
+                <h4 class="modal-title"><b>Add New Category</b></h4>
             </div>
             <div class="modal-body">
-              <form class="form-horizontal" method="POST" action="category_add.php">
-                <div class="form-group">
-                    <label for="name" class="col-sm-3 control-label">Name</label>
-
-                    <div class="col-sm-9">
-                      <input type="text" class="form-control" id="name" name="name" required>
+                <form class="form-horizontal" method="POST" action="category_add.php">
+                    <div class="form-group">
+                        <label for="name" class="col-sm-3 control-label">Name</label>
+                        <div class="col-sm-9">
+                            <input type="text" class="form-control" id="name" name="name" required>
+                        </div>
                     </div>
-                </div>
+                    <div class="form-group">
+                        <label for="subcategories" class="col-sm-3 control-label">Subcategories</label>
+                        <div class="col-sm-9">
+                            <input type="text" class="form-control" id="subcategories" name="subcategories" placeholder="Enter subcategories, separated by commas (e.g., Shirts, Pants)">
+                        </div>
+                    </div>
             </div>
             <div class="modal-footer">
-              <button type="button" class="btn btn-default btn-flat pull-left" data-dismiss="modal"><i class="fa fa-close"></i> Close</button>
-              <button type="submit" class="btn btn-primary btn-flat" name="add"><i class="fa fa-save"></i> Save</button>
-              </form>
+                <button type="button" class="btn btn-default btn-flat pull-left" data-dismiss="modal"><i class="fa fa-close"></i> Close</button>
+                <button type="submit" class="btn btn-primary btn-flat" name="add"><i class="fa fa-save"></i> Save</button>
+                </form>
             </div>
         </div>
     </div>
@@ -31,25 +36,30 @@
     <div class="modal-dialog">
         <div class="modal-content">
             <div class="modal-header">
-              <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                  <span aria-hidden="true">&times;</span></button>
-              <h4 class="modal-title"><b>Edit Category</b></h4>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">×</span></button>
+                <h4 class="modal-title"><b>Edit Category</b></h4>
             </div>
             <div class="modal-body">
-              <form class="form-horizontal" method="POST" action="category_edit.php">
-                <input type="hidden" class="catid" name="id">
-                <div class="form-group">
-                    <label for="edit_name" class="col-sm-3 control-label">Name</label>
-
-                    <div class="col-sm-9">
-                      <input type="text" class="form-control" id="edit_name" name="name">
+                <form class="form-horizontal" method="POST" action="category_edit.php">
+                    <input type="hidden" class="catid" name="id">
+                    <div class="form-group">
+                        <label for="edit_name" class="col-sm-3 control-label">Name</label>
+                        <div class="col-sm-9">
+                            <input type="text" class="form-control" id="edit_name" name="name" required>
+                        </div>
                     </div>
-                </div>
+                    <div class="form-group">
+                        <label for="edit_subcategories" class="col-sm-3 control-label">Subcategories</label>
+                        <div class="col-sm-9">
+                            <input type="text" class="form-control" id="edit_subcategories" name="subcategories" placeholder="Enter subcategories, separated by commas (e.g., Shirts, Pants)">
+                        </div>
+                    </div>
             </div>
             <div class="modal-footer">
-              <button type="button" class="btn btn-default btn-flat pull-left" data-dismiss="modal"><i class="fa fa-close"></i> Close</button>
-              <button type="submit" class="btn btn-success btn-flat" name="edit"><i class="fa fa-check-square-o"></i> Update</button>
-              </form>
+                <button type="button" class="btn btn-default btn-flat pull-left" data-dismiss="modal"><i class="fa fa-close"></i> Close</button>
+                <button type="submit" class="btn btn-success btn-flat" name="edit"><i class="fa fa-check-square-o"></i> Update</button>
+                </form>
             </div>
         </div>
     </div>
@@ -60,22 +70,22 @@
     <div class="modal-dialog">
         <div class="modal-content">
             <div class="modal-header">
-              <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                  <span aria-hidden="true">&times;</span></button>
-              <h4 class="modal-title"><b>Deleting...</b></h4>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">×</span></button>
+                <h4 class="modal-title"><b>Deleting...</b></h4>
             </div>
             <div class="modal-body">
-              <form class="form-horizontal" method="POST" action="category_delete.php">
-                <input type="hidden" class="catid" name="id">
-                <div class="text-center">
-                    <p>DELETE CATEGORY</p>
-                    <h2 class="bold catname"></h2>
-                </div>
+                <form class="form-horizontal" method="POST" action="category_delete.php">
+                    <input type="hidden" class="catid" name="id">
+                    <div class="text-center">
+                        <p>DELETE CATEGORY</p>
+                        <h2 class="bold catname"></h2>
+                    </div>
             </div>
             <div class="modal-footer">
-              <button type="button" class="btn btn-default btn-flat pull-left" data-dismiss="modal"><i class="fa fa-close"></i> Close</button>
-              <button type="submit" class="btn btn-danger btn-flat" name="delete"><i class="fa fa-trash"></i> Delete</button>
-              </form>
+                <button type="button" class="btn btn-default btn-flat pull-left" data-dismiss="modal"><i class="fa fa-close"></i> Close</button>
+                <button type="submit" class="btn btn-danger btn-flat" name="delete"><i class="fa fa-trash"></i> Delete</button>
+                </form>
             </div>
         </div>
     </div>
