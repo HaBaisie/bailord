e6f662dceac14417d2ab33eab70a7eb81e43f7bc
BaisieTech <57533856+HaBaisie@users.noreply.github.com>
2025-07-13 17:50:52 +0100

    Update config.php

--- config.php
+++ config.php
@@ -5,6 +5,8 @@ define('KWIK_EMAIL', 'lawalhabeeb3191@gmail.com');
 define('KWIK_PASSWORD', 'Kwik2025$');
 define('KWIK_BASE_URL', 'https://staging-api-test.kwik.delivery');
 define('KWIK_DOMAIN', 'staging-client-panel.kwik.delivery');
+define('domain_name', 'staging-client-panel.kwik.delivery');
+define('api_login', 1);
 define('MAPBOX_ACCESS_TOKEN', 'pk.eyJ1IjoiaGFiYWlzaWUiLCJhIjoiY21kMWpjcHp2MTVtajJtcW5kcmo2ZTJ2OCJ9.pS4iUcgLoJITbyg-1CXl5w');
 define('PAYSTACK_PUBLIC_KEY', 'pk_test_79848b3271a3d80eef6a4c34d9d84f00d7a46dcb');
 ?>
