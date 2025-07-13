aa3866c2d2ebf82f04ba315c726c03f31bf9e24e
BaisieTech <57533856+HaBaisie@users.noreply.github.com>
2025-07-13 15:49:55 +0100

    Add files via upload

---
+++ config.php
@@ -0,0 +1,10 @@
+<?php
+// config.php
+// Store sensitive data like Kwik API credentials
+define('KWIK_EMAIL', 'lawalhabeeb3191@gmail.com');
+define('KWIK_PASSWORD', 'Kwik2025$');
+define('KWIK_BASE_URL', 'https://staging-api-test.kwik.delivery');
+define('KWIK_DOMAIN', 'staging-client-panel.kwik.delivery');
+define('MAPBOX_ACCESS_TOKEN', 'pk.eyJ1IjoiaGFiYWlzaWUiLCJhIjoiY21kMWpjcHp2MTVtajJtcW5kcmo2ZTJ2OCJ9.pS4iUcgLoJITbyg-1CXl5w');
+define('PAYSTACK_PUBLIC_KEY', 'pk_test_79848b3271a3d80eef6a4c34d9d84f00d7a46dcb');
+?>
