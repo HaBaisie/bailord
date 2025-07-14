9154b8bb057dd852b678dfaffda3b3239872358e
BaisieTech <57533856+HaBaisie@users.noreply.github.com>
2025-07-14 21:45:56 +0100

    Update profile_modal.php

--- includes/profile_modal.php
+++ includes/profile_modal.php
@@ -1,113 +1,150 @@
-<!-- Transaction History -->
-<div class="modal fade" id="transaction">
-    <div class="modal-dialog">
+<?php
+// Ensure $user is available; include session if not already included
+if (!isset($user)) {
+    include 'includes/session.php';
+}
+?>
+
+<!-- Transaction History Modal -->
+<div class="modal fade" id="transaction" tabindex="-1" role="dialog" aria-labelledby="transactionLabel" aria-hidden="true">
+    <div class="modal-dialog modal-dialog-centered" role="document">
         <div class="modal-content">
             <div class="modal-header">
-              <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                  <span aria-hidden="true">&times;</span></button>
-              <h4 class="modal-title"><b>Transaction Full Details</b></h4>
+                <h4 class="modal-title" id="transactionLabel"><b>Transaction Full Details</b></h4>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
             </div>
             <div class="modal-body">
-              <p>
-                Date: <span id="date"></span>
-                <span class="pull-right">Transaction#: <span id="transid"></span></span> 
-              </p>
-              <table class="table table-bordered">
-                <thead>
-                  <th>Product</th>
-                  <th>Price</th>
-                  <th>Quantity</th>
-                  <th>Subtotal</th>
-                </thead>
-                <tbody id="detail">
-                  <tr>
-                    <td colspan="3" align="right"><b>Total</b></td>
-                    <td><span id="total"></span></td>
-                  </tr>
-                </tbody>
-              </table>
+                <div class="row">
+                    <div class="col-sm-6">
+                        <p>Date: <span id="date"></span></p>
+                    </div>
+                    <div class="col-sm-6 text-right">
+                        <p>Transaction#: <span id="transid"></span></p>
+                    </div>
+                </div>
+                <table class="table table-bordered">
+                    <thead>
+                        <tr>
+                            <th>Product</th>
+                            <th>Price</th>
+                            <th>Quantity</th>
+                            <th>Subtotal</th>
+                        </tr>
+                    </thead>
+                    <tbody id="detail">
+                        <tr class="prepend_items">
+                            <td colspan="3" align="right"><b>Total</b></td>
+                            <td><span id="total"></span></td>
+                        </tr>
+                    </tbody>
+                </table>
             </div>
             <div class="modal-footer">
-              <button type="button" class="btn btn-default btn-flat pull-left" data-dismiss="modal"><i class="fa fa-close"></i> Close</button>
+                <button type="button" class="btn btn-default btn-flat" data-dismiss="modal"><i class="fa fa-close"></i> Close</button>
             </div>
         </div>
     </div>
 </div>
 
-<!-- Edit Profile -->
-<div class="modal fade" id="edit">
-    <div class="modal-dialog">
+<!-- Track Order Modal -->
+<div class="modal fade" id="trackOrder" tabindex="-1" role="dialog" aria-labelledby="trackOrderLabel" aria-hidden="true">
+    <div class="modal-dialog modal-dialog-centered" role="document">
         <div class="modal-content">
             <div class="modal-header">
-              <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                  <span aria-hidden="true">&times;</span></button>
-              <h4 class="modal-title"><b>Update Account</b></h4>
+                <h4 class="modal-title" id="trackOrderLabel"><b>Track Order</b></h4>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
             </div>
             <div class="modal-body">
-              <form class="form-horizontal" method="POST" action="profile_edit.php" enctype="multipart/form-data">
-                <div class="form-group">
-                    <label for="firstname" class="col-sm-3 control-label">Firstname</label>
-
-                    <div class="col-sm-9">
-                      <input type="text" class="form-control" id="firstname" name="firstname" value="<?php echo $user['firstname']; ?>">
+                <div class="row">
+                    <div class="col-sm-3">
+                        <h4>Order ID:</h4>
+                        <h4>Tracking Link:</h4>
+                        <h4>Created At:</h4>
                     </div>
-                </div>
-                <div class="form-group">
-                    <label for="lastname" class="col-sm-3 control-label">Lastname</label>
-
                     <div class="col-sm-9">
-                      <input type="text" class="form-control" id="lastname" name="lastname" value="<?php echo $user['lastname']; ?>">
-                    </div>
-                </div>
-                <div class="form-group">
-                    <label for="email" class="col-sm-3 control-label">Email</label>
-
-                    <div class="col-sm-9">
-                      <input type="text" class="form-control" id="email" name="email" value="<?php echo $user['email']; ?>">
-                    </div>
-                </div>
-                <div class="form-group">
-                    <label for="password" class="col-sm-3 control-label">Password</label>
-
-                    <div class="col-sm-9">
-                      <input type="password" class="form-control" id="password" name="password" value="<?php echo $user['password']; ?>">
-                    </div>
-                </div>
-                <div class="form-group">
-                    <label for="contact" class="col-sm-3 control-label">Contact Info</label>
-
-                    <div class="col-sm-9">
-                      <input type="text" class="form-control" id="contact" name="contact" value="<?php echo $user['contact_info']; ?>">
-                    </div>
-                </div>
-                <div class="form-group">
-                    <label for="address" class="col-sm-3 control-label">Address</label>
-
-                    <div class="col-sm-9">
-                      <textarea class="form-control" id="address" name="address"><?php echo $user['address']; ?></textarea>
-                    </div>
-                </div>
-                <div class="form-group">
-                    <label for="photo" class="col-sm-3 control-label">Photo</label>
-
-                    <div class="col-sm-9">
-                      <input type="file" id="photo" name="photo">
-                    </div>
-                </div>
-                <hr>
-                
-                <div class="form-group">
-                    <label for="curr_password" class="col-sm-3 control-label">Current Password</label>
-
-                    <div class="col-sm-9">
-                      <input type="password" class="form-control" id="curr_password" name="curr_password" placeholder="input current password to save changes" required>
+                        <h4 id="track_order_id"></h4>
+                        <h4 id="track_link"></h4>
+                        <h4 id="track_created"></h4>
                     </div>
                 </div>
             </div>
             <div class="modal-footer">
-              <button type="button" class="btn btn-default btn-flat pull-left" data-dismiss="modal"><i class="fa fa-close"></i> Close</button>
-              <button type="submit" class="btn btn-success btn-flat" name="edit"><i class="fa fa-check-square-o"></i> Update</button>
-              </form>
+                <button type="button" class="btn btn-default btn-flat" data-dismiss="modal"><i class="fa fa-close"></i> Close</button>
+            </div>
+        </div>
+    </div>
+</div>
+
+<!-- Edit Profile Modal -->
+<div class="modal fade" id="edit" tabindex="-1" role="dialog" aria-labelledby="editProfileLabel" aria-hidden="true">
+    <div class="modal-dialog modal-dialog-centered" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h4 class="modal-title" id="editProfileLabel"><b>Update Account</b></h4>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div class="modal-body">
+                <form class="form-horizontal" method="POST" action="profile_edit.php" enctype="multipart/form-data">
+                    <div class="form-group">
+                        <label for="firstname" class="col-sm-3 control-label">Firstname</label>
+                        <div class="col-sm-9">
+                            <input type="text" class="form-control" id="firstname" name="firstname" value="<?php echo isset($user['firstname']) ? htmlspecialchars($user['firstname']) : ''; ?>" required>
+                        </div>
+                    </div>
+                    <div class="form-group">
+                        <label for="lastname" class="col-sm-3 control-label">Lastname</label>
+                        <div class="col-sm-9">
+                            <input type="text" class="form-control" id="lastname" name="lastname" value="<?php echo isset($user['lastname']) ? htmlspecialchars($user['lastname']) : ''; ?>" required>
+                        </div>
+                    </div>
+                    <div class="form-group">
+                        <label for="email" class="col-sm-3 control-label">Email</label>
+                        <div class="col-sm-9">
+                            <input type="email" class="form-control" id="email" name="email" value="<?php echo isset($user['email']) ? htmlspecialchars($user['email']) : ''; ?>" required>
+                        </div>
+                    </div>
+                    <div class="form-group">
+                        <label for="password" class="col-sm-3 control-label">New Password</label>
+                        <div class="col-sm-9">
+                            <input type="password" class="form-control" id="password" name="password" placeholder="Leave blank to keep current password">
+                        </div>
+                    </div>
+                    <div class="form-group">
+                        <label for="contact" class="col-sm-3 control-label">Contact Info</label>
+                        <div class="col-sm-9">
+                            <input type="text" class="form-control" id="contact" name="contact" value="<?php echo isset($user['contact_info']) ? htmlspecialchars($user['contact_info']) : ''; ?>">
+                        </div>
+                    </div>
+                    <div class="form-group">
+                        <label for="address" class="col-sm-3 control-label">Address</label>
+                        <div class="col-sm-9">
+                            <textarea class="form-control" id="address" name="address"><?php echo isset($user['address']) ? htmlspecialchars($user['address']) : ''; ?></textarea>
+                        </div>
+                    </div>
+                    <div class="form-group">
+                        <label for="photo" class="col-sm-3 control-label">Photo</label>
+                        <div class="col-sm-9">
+                            <input type="file" id="photo" name="photo" accept="image/*">
+                        </div>
+                    </div>
+                    <hr>
+                    <div class="form-group">
+                        <label for="curr_password" class="col-sm-3 control-label">Current Password</label>
+                        <div class="col-sm-9">
+                            <input type="password" class="form-control" id="curr_password" name="curr_password" placeholder="Input current password to save changes" required>
+                        </div>
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-default btn-flat" data-dismiss="modal"><i class="fa fa-close"></i> Close</button>
+                        <button type="submit" class="btn btn-success btn-flat" name="edit"><i class="fa fa-check-square-o"></i> Update</button>
+                    </div>
+                </form>
             </div>
         </div>
     </div>
